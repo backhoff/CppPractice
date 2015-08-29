63f9d706d9e8472399995249d37648bbd2004da2
Guillermo Garcia <g.backhoff@hotmail.com>
2015-08-29 18:04:28 -0500

    SPOJ 15711 Wow

---
+++ wow.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    int n;
+    cin >> n;
+    string letter = string(n, 'o');     // Letter O will be printed N times
+    cout << "W" << letter << "w" << endl;
+    return 0;
+}
+
+
